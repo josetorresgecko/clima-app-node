0f6b23e7c0b6d1dddd84aad2a317fb79b7ba939e
josetorresgecko <jose.torres@gecko.com.co>
2020-08-17 09:41:48 -0500

    appid security

--- clima/clima.js
+++ clima/clima.js
@@ -1,10 +1,10 @@
 const axios = require('axios');
 const getClima = async(lat, lng) => {
-    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=3f63802f12783fe78c409dc25c7ff65e&units=metric`);
+    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=*********&units=metric`);
     return resp;
 };
 const getClimaCity = async(city) => {
-    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3f63802f12783fe78c409dc25c7ff65e&units=metric`);
+    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=*******&units=metric`);
     if (resp.status == 200) {
         return resp;
     } else {
